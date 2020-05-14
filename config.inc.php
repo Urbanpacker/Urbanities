b49289f3b213f2517fd8e0471fe70fb59f634e7f
Urbanpacker <dev@urbanities.fr>
2020-05-14 18:21:44 +0200

    Refactorisation des methodes de manipluation de carte OSM dans fichier mapCreator.js

--- config.inc.php
+++ config.inc.php
@@ -2,9 +2,9 @@
 
 // The db connecting data have been removed in order to preserve the db security
 
-const BDD_SGBD      = '***************';
-const BDD_HOST      = '***************';
+const BDD_SGBD      = 'mysql';
+const BDD_HOST      = 'localhost';
 const BDD_PORT	    = '***************';
-const BDD_DATABASE  = '***************';
-const BDD_USER		= '***************';
-const BDD_PASSWORD	= '***************';
+const BDD_DATABASE  = 'urbanities';
+const BDD_USER		= 'jules';
+const BDD_PASSWORD	= 'JvtaMsI09';
--- public/js/moduls/mapCreator.js
+++ public/js/moduls/mapCreator.js
@@ -27,14 +27,28 @@
 				long : long
 			};
 			this.positions = [this.coords] ;
+			this.selectMapMenu ;
+
+			/* Add the stored spot coordinates to the currently handled object UrbanMap */
+			let addingCoords = loadDataFromDomStorage("savedCoords", "session");
+			if(addingCoords){
+				this.addPosition(addingCoords);
+			}
+
+			/* Gets the user's preferred tilelayer from the client DOM Storage */
+			this.setTileLayer();
+			/* Create an OSM Map with the preferred tilelayer*/
+			this.setOSMMap(this.mapTileLayer);
+			this.setMarker();
 		}
+
 		setOSMMap = (tileLayer = this.mapTileLayers.default) => {
-			// Checks that a container has been set
+			/* Checks that a container has been set*/
 			if(!this.mapContainer){
 				console.log("Pas de conteneur défini pour la carte")
 				return;
 			}
-			// Checks that no map has already been set in the container
+			/* Checks that no map has already been set in the container */
 			if(this.mapContainer.hasChildNodes()){
 				let el = this.mapContainer.childNodes ;
 				for(let i = 0, c = el.length; i < c; ++i){
@@ -44,10 +58,10 @@
 					}
 				}
 			}
-			// Initializes a OSM Map with options
+			/* Initializes a OSM Map with options */
 			this.leafletMap = L.map(this.mapContainer.id).setView(this.zoomPoint, this.zoomDegree);
 			this.mapContainer.classList.add("map");
-			// Loads the OSM tileLayer (map background) */
+			/* Loads the OSM tileLayer (map background) */
 			L.tileLayer(tileLayer,{
 				maxzoom:19,
 				attribution:'(c)<a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
@@ -58,13 +72,83 @@
 			this.positions.push(...position) ;
 		}
 
+		/*Set markers onto the displayed OSM Map */
 		setMarker = (lat = this.coords.lat, long = this.coords.long)=>{
-			L.marker([lat,long]).addTo(this.leafletMap);
+				/*Triggers the "several markers setting" method if there are saved positions */		
+				if(this.positions.length > 1){
+					this.setSeveralMarkers(this.positions);
+				} else {
+					L.marker([lat,long]).addTo(this.leafletMap);
+			}
 		}
+
 		setSeveralMarkers = (positions) =>{
 			for(let i = 0, c = positions.length ; i<c ; i++){
 				L.marker([positions[i].lat,positions[i].long]).addTo(this.leafletMap);
 			}	
+		}
+
+		saveCoordsToDOMStorage = () => {
+			let savedCoords = loadDataFromDomStorage("savedCoords", "session");
+			removeDataFromDomStorage("savedCoords", "session");
+			let coordsPresentInStorage = false;
+			/*If the DOM storage is empty, we create a new array with the currentspot position as a first element */
+			if(savedCoords === null){
+				savedCoords = [];
+				savedCoords.push({lat : this.coords.lat, long : this.coords.long});
+			} else{
+			/* We check that the current spot position has not been saved in the DOM storage yet */
+				for(let i = 0, c = savedCoords.length ; i < c ; i++){
+					if(((savedCoords[i].lat === this.coords.lat) && (savedCoords[i].long === this.coords.long))){
+						coordsPresentInStorage = true ;		
+					}
+				}
+			/* If the current position has not been stored, we put it into the storage */
+				if(!coordsPresentInStorage){
+					savedCoords.push({lat : this.coords.lat, long : this.coords.long})
+				}
+			}
+			saveDataToDomStorage("savedCoords", savedCoords, "session");
+		}
+
+		setTileLayer = () => {
+			/* Put every tilelayer of a the created UrbanMap into an array in order to use of it later easily */
+			var tileLayers = [] ;
+			for(let prop in this.mapTileLayers){
+				tileLayers.push(this.mapTileLayers[prop]);
+			}
+
+			/* Load the user chosen tilelayer and set it */
+			let preferedMap = loadDataFromDomStorage("preferedMapTileLayer", "local") || 0;
+			this.mapTileLayer = tileLayers[preferedMap];
+			
+			/****Scrolling menu to choose a map tilelayer */
+			let selectMapMenu = document.createElement("select");
+			let optNumber = 0 ;
+			for(let prop in this.mapTileLayers){
+				let opt = document.createElement("option");
+				opt.value = optNumber;
+				opt.innerText = ucFirst(prop);
+				if(optNumber === preferedMap){
+					opt.setAttribute("selected", "");
+				}
+				selectMapMenu.appendChild(opt);
+				++optNumber;
+			}
+			this.selectMapMenu = selectMapMenu;
+
+			/* Event listener watching the change of map tilelayer by the user. On change, replace the preferred tile layer in the local DOM Storage */
+			selectMapMenu.onchange = (e) => {
+				let preferedMapTileLayer = 0;
+				for (let i =0, c = tileLayers.length ; i< c ; i++){
+					if(i === e.target.selectedIndex){
+						preferedMapTileLayer = i;
+					}
+				}
+				removeDataFromDomStorage("preferedMapTileLayer", "local");
+				saveDataToDomStorage("preferedMapTileLayer", preferedMapTileLayer, "local");
+				location.reload(true);
+			};
 		}
 	}
 
